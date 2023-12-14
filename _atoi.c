81e80422eb666588f7898844dd57e685e27e4564
ouchafra <ismail.soso9@gmail.com>
2023-12-14 03:51:56 +0100

    0x16. C - Simple Shell

--- _atoi.c
+++ _atoi.c
@@ -8,7 +8,7 @@
  */
 int interactive(info_t *info)
 {
-    return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -20,10 +20,10 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-    while (*delim)
-        if (*delim++ == c)
-            return (1);
-    return (0);
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
@@ -34,7 +34,7 @@ int is_delim(char c, char *delim)
  */
 int _isalpha(int c)
 {
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
 /**
@@ -45,24 +45,24 @@ int _isalpha(int c)
  */
 int _atoi(char *s)
 {
-    int i, sign = 1, flag = 0, output;
-    unsigned int result = 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-    for (i = 0; s[i] != '\0' && flag != 2; i++)
-    {
-        if (s[i] == '-')
-            sign *= -1;
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            flag = 1;
-            result *= 10;
-            result += (s[i] - '0');
-        }
-        else if (flag == 1)
-            flag = 2;
-    }
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
 
-    output = (sign == -1) ? -result : result;
-    return (output);
+	output = (sign == -1) ? -result : result;
+	return (output);
 }
